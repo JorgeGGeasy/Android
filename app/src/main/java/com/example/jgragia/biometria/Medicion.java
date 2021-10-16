e48b2b6893d32f5138116583f4cc2c2d152e8fe2
Jorge <jgragia@epsg.upv.es>
2021-10-16 15:21:09 +0200

    Clase medicion

--- app/src/main/java/com/example/jgragia/biometria/Medicion.java
+++ app/src/main/java/com/example/jgragia/biometria/Medicion.java
@@ -1,4 +1,48 @@
 package com.example.jgragia.biometria;
 
 public class Medicion {
+    
+    private double valor;
+    private double latitud;
+    private double longitud;
+    private String fecha;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(int latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public Medicion(double valor, double latitud, double longitud, String fecha) {
+        this.valor = valor;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.fecha = fecha;
+    }
 }
